ad575cf89515b6b5c74e1d24d5be767493b0e60c
DeuMiAn <66052290+DeuMiAn@users.noreply.github.com>
2023-04-19 19:51:12 +0900

    add

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,20 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+//jpa는 인터페이스임
+//자바 표준 인터페이스임
+//객체와 ORM 기술 즉 어노테이션 으로 한다
+
+@Entity
 public class Member { //회원관련 도메인
+
+    //GenerationType.IDENTITY 는 DB가 알아서 ID생성
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     public Long getId() {
--- src/test/java/hello/hellospring/service/MemberServiceIntergrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntergrationTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -25,10 +26,11 @@ class MemberServiceIntergrationTest {
     MemberRepository memberRepository;
 
     @Test
+//    @Commit
     void 회원가입() {
-        //given
+        //givenÍ
         Member member = new Member();
-        member.setName("spring");
+        member.setName("spring110");
 
         //when
         Long saveId=memberService.join(member);
