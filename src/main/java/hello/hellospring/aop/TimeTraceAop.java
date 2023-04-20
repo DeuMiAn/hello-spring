f8a78f031aa218940ca5a6bc2c0c27a8b0a99732
DeuMiAn <66052290+DeuMiAn@users.noreply.github.com>
2023-04-20 19:52:21 +0900

    add

--- src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -6,12 +6,13 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
-@Component
+//@Component
 public class TimeTraceAop {
     //중간에 인터셉팅해서 풇어내는 기술이 AOP임
 
 
-    @Around("execution(* hello.hellospring..*(..))")
+    @Around("execution(* hello.hellospring..*(..)) && !target(hello.hellospring.SpringConfig)")
+//    @Around("execution(* hello.hellospring..*(..))")  //이건 컴포넌트 어노테이션일때
     public Object execute(ProceedingJoinPoint joinPoint) throws  Throwable{
         long start = System.currentTimeMillis();
         System.out.println("START: " + joinPoint.toString());
--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+//JpaRepository 와 interface 로 구성시 자동으로 스프링 been에 등록됨
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
     @Override
